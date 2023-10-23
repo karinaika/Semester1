de44d5e53eead63c159b0689f0f32d8b371928ac
karinaika <karinaikaindasa@gmail.com>
2023-10-23 21:49:06 +0700

    first init github

---
+++ jobsheet6/Pemilihan2Percobaan115.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan115 {
+    public static void main(String[] args){
+        Scanner input15 = new Scanner(System.in);
+        int tahun;
+        System.out.print("Masukkan Tahun");
+        tahun=input15.nextInt();
+        
+        tahun=0;
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
