a462dcea254345029c4950fbaf7a38aac3f8b94d
karinaika <karinaikaindasa@gmail.com>
2023-10-24 12:41:37 +0700

    kode modifikasi

--- jobsheet6/Pemilihan2Percobaan115.java
+++ jobsheet6/Pemilihan2Percobaan115.java
@@ -3,13 +3,16 @@ public class Pemilihan2Percobaan115 {
     public static void main(String[] args){
         Scanner input15 = new Scanner(System.in);
         int tahun;
-        System.out.print("Masukkan Tahun");
+        System.out.print("Masukkan Tahun: ");
         tahun=input15.nextInt();
         
         tahun=0;
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0){
                 System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
