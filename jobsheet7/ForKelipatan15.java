001e7e6e08fdcb1b9ccd04e046a73d8890616f16
karinaika <karinaikaindasa@gmail.com>
2023-10-30 20:59:04 +0700

    kode modifikasi

--- jobsheet7/ForKelipatan15.java
+++ jobsheet7/ForKelipatan15.java
@@ -4,9 +4,10 @@ public class ForKelipatan15 {
     public static void main(String[] args ){
         Scanner scan = new Scanner(System.in);
         int kelipatan, jumlah, counter;
+        float rata;
         jumlah=0;
         counter=0;
-
+        rata=0;
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan=scan.nextInt();
         
@@ -16,7 +17,9 @@ public class ForKelipatan15 {
                 counter++;
             }
         }
+            rata = (float)jumlah/counter;
             System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
             System.out.printf("Total bilangan keliapatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+            System.out.printf("Rata-rata bilangan keliapatan %d dari 1 sampai 50 adalah %.2f\n",kelipatan, rata);
     }
 }
