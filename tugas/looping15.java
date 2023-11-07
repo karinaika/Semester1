bc0aff14c7ad3b52f64642e739abf1b7e2142f8b
karinaika <karinaikaindasa@gmail.com>
2023-11-07 19:48:30 +0700

    first init github

---
+++ tugas/looping15.java
@@ -0,0 +1,16 @@
+public class looping15 {
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < n - i + 1;) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
