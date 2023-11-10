ba70003474c55f2d1300e402972ad49e032dcfa4
karinaika <karinaikaindasa@gmail.com>
2023-11-10 22:38:46 +0700

    kode modifikasi

--- jobsheet9/ArrayRataNilai15.java
+++ jobsheet9/ArrayRataNilai15.java
@@ -3,19 +3,43 @@ import java.util.Scanner;
 public class ArrayRataNilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs= new int[10];
-        double total = 0;
-        double rata2;
+        // int[] nilaiMhs= new int[10];
+        double total = 0, rata2, rataLulus = 0, rataTdkLulus = 0, totLulus = 0, totTdkLulus = 0;
+        int jmlMhs, jmlLulus = 0, jmlTdkLulus = 0;
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
         
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
-        }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];   
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
 
+            // if (nilaiMhs[i] > 70) {
+            //     jmlLulus++;
+            // }
+        }
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+           total += nilaiMhs[i];
+        }
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                jmlLulus++;
+                totLulus += nilaiMhs[i];
+            } else {
+                jmlTdkLulus++;
+                totTdkLulus += nilaiMhs[i];
+            }
+        }        
+
+        rataLulus = totLulus/jmlLulus;
+        rataTdkLulus = totTdkLulus/jmlTdkLulus;
+        System.out.println("Rata-rata nilai lulus : "+rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus : "+rataTdkLulus);
+        // rata2 = total/nilaiMhs.length;
+        // System.out.println("Rata-rata nilai = "+rata2);
+        // System.out.println("Jumlah mahasiswa yang lulus = "+jmlLulus);
     }
 }
