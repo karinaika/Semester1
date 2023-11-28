ea61330d9ab6a36eedfaf56de087dab9a10981e7
karinaika <karinaikaindasa@gmail.com>
2023-11-28 08:35:05 +0700

    commit

---
+++ jobsheet11/NesteedLoop15.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NesteedLoop15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[][] temps= new double[5][7];
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("City: " + i);
+            for (int j = 0; j < temps.length; j++) {
+                System.out.print("Day " + (j+1) + ": "); 
+                temps[i][j] = scanner.nextDouble();
+            }
+           System.out.println();
+        }
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("City: " + i);
+            for (int j = 0; j < temps.length; j++) {
+                System.out.print(temps[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
